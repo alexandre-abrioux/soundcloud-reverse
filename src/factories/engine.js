8abc3c56396620719a6e3cf83e488421a1e2d74b
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2021-12-17 00:21:57 +0100

    fix: use alexandre-abrioux/soundcloud-audio.js

--- src/factories/engine.js
+++ src/factories/engine.js
@@ -2,10 +2,9 @@ const app = require("../app");
 
 app.factory("engine", [
     "settings",
-    "sc-params",
-    function (settings, scParams) {
+    function (settings) {
         const engine = {
-            player: new SoundCloudAudio(scParams.client_id),
+            player: null,
             audioCtx: new (window.AudioContext || window.webkitAudioContext)(),
             audioSrc: null,
             frequencyData: null,
@@ -13,10 +12,6 @@ app.factory("engine", [
             maxFrequencyInArray: 0,
             nbValuesToKeepInArray: 0,
         };
-        engine.player.audio.crossOrigin = "anonymous";
-        engine.audioSrc = engine.audioCtx.createMediaElementSource(
-            engine.player.audio
-        );
         engine.analyser = engine.audioCtx.createAnalyser();
         engine.analyser.fftSize = Math.pow(2, 15);
         engine.analyser.minDecibels = -70;
@@ -24,8 +19,6 @@ app.factory("engine", [
         engine.analyser.smoothingTimeConstant = settings.smoothing / 100;
         engine.gainNode = engine.audioCtx.createGain();
         engine.gainNode.connect(engine.audioCtx.destination);
-        engine.audioSrc.connect(engine.analyser);
-        engine.audioSrc.connect(engine.gainNode);
         return engine;
     },
 ]);
