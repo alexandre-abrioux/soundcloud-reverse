5b9c9f5103b568652d9024c39121da117a064596
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2025-02-13 02:00:22 +0100

    remove vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const LoginHtmlPlugin = {
-  name: "loginMiddleware",
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.originalUrl.indexOf("/login") === 0) {
-        req.url = "/login/index.html";
-      }
-      next();
-    });
-  },
-};
-
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   // https://vitejs.dev/config/
@@ -21,7 +9,6 @@ export default ({ mode }) => {
     server: { port: 3000, host: true },
     preview: { port: 3000, host: true },
     plugins: [
-      // LoginHtmlPlugin,
       react({
         jsxImportSource: "@emotion/react",
         babel: {
